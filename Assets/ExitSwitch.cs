1d1db1b555fe7ee0fa2e0f69f2f7f5d59d5cc6ce
quomitter <109629602+quomitter@users.noreply.github.com>
2022-09-03 14:51:27 -0400

    update

---
+++ Assets/ExitSwitch.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ExitSwitch : MonoBehaviour
+{
+
+    public UnityEvent doorswitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(doorswitch == null){
+            doorswitch = new UnityEvent(); 
+        }
+        doorswitch.AddListener(DoorSwitch);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DoorSwitch(){
+        Debug.Log("you have escaped");
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "Player" && Input.GetButtonDown("Fire1"))
+        {
+            doorswitch.Invoke();
+        }
+    }    
+
+    public void OpenDoor(){
+            doorswitch.Invoke();
+    } 
+}
