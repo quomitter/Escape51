b45c6388775d653eb799bce51c01c629c2e9ee94
quomitter <109629602+quomitter@users.noreply.github.com>
2022-09-06 06:38:07 -0400

    updates

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,16 +6,16 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    
+
     AudioSource audioSource;
-    public AudioClip jumpSound; 
-    public AudioClip gunSound;  
+    public AudioClip jumpSound;
+    public AudioClip gunSound;
     public Animator anim;
 
     public Rigidbody2D playerRB;
     public GameObject laserShot;
     public Transform firePoint;
-    public TMP_Text deathText; 
+    public TMP_Text deathText;
 
     public Vector2 direction;
 
@@ -37,22 +37,22 @@ public class PlayerController : MonoBehaviour
     public float canRollTimeLeft = 1f;
 
     public bool isDead;
-    public bool isInUI; 
+    public bool isInUI;
     public PlayerHealthController playerHealthController;
     public float isDeadCounter;
     public bool isInAir;
     public float inAirCounter;
 
     int activeSceneIndex;
-    public bool hasRescuedBlue; 
+    public bool hasRescuedBlue;
 
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>(); 
-        hasRescuedBlue = false; 
-        activeSceneIndex = SceneManager.GetActiveScene().buildIndex; 
-        isInUI = false; 
+        audioSource = GetComponent<AudioSource>();
+        hasRescuedBlue = false;
+        activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        isInUI = false;
         jumpCounter = 0;
         isDeadCounter = 0;
         canRoll = true;
@@ -66,103 +66,105 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!isInUI){
-        if (!isDead)
+        if (!isInUI)
         {
-            playerRB.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, playerRB.velocity.y);
-
-            if (playerRB.velocity.x > 0 && m_FacingRight)
+            if (!isDead)
             {
-                Flip();
-            }
-            else if (playerRB.velocity.x < 0 && !m_FacingRight)
-            {
-                Flip();
-            }
+                playerRB.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, playerRB.velocity.y);
 
-            if (Input.GetButtonDown("Jump") && jumpCounter < 1)
-            {
-                audioSource.PlayOneShot(jumpSound, 0.35f);
-                jumpCounter++;
-                playerRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
-            }
-
-
-            if (Input.GetButton("Fire1") && !isRolling)
-            {
-                anim.SetBool("isShooting", true);
-                if (Time.time > fireRate + lastShot)
+                if (playerRB.velocity.x > 0 && m_FacingRight)
                 {
-                    audioSource.PlayOneShot(gunSound, 0.35f);
-                    GameObject clone = Instantiate(laserShot, firePoint.position, firePoint.rotation);
-                    Physics2D.IgnoreCollision(clone.GetComponent<Collider2D>(), playerRB.GetComponent<Collider2D>());
-                    Rigidbody2D shot = clone.GetComponent<Rigidbody2D>();
-                    if (!m_FacingRight)
-                        shot.AddForce(transform.right * bulletSpeed, ForceMode2D.Impulse);
-                    else
-                        shot.AddForce(-transform.right * bulletSpeed, ForceMode2D.Impulse);
-                    Destroy(clone.gameObject, 1f);
-                    lastShot = Time.time;
+                    Flip();
                 }
-            }
-            else
-            {
-                anim.SetBool("isShooting", false);
-            }
-
-
-            if (Input.GetButtonUp("Fire2"))
-            {
-                canRollTimer = canRollTimeLeft;
-            }
-            if (Input.GetButton("Fire2") && canRollTimer > 0 && canRollCoolDown < 0)
-            {
-                canRollTimer -= Time.deltaTime;
-                anim.SetBool("isRolling", true);
-                isRolling = true;
-                if (canRollTimer < 0)
+                else if (playerRB.velocity.x < 0 && !m_FacingRight)
                 {
-                    canRollCoolDown = 1f;
+                    Flip();
+                }
+
+                if (Input.GetButtonDown("Jump") && jumpCounter < 1)
+                {
+                    audioSource.PlayOneShot(jumpSound, 0.35f);
+                    jumpCounter++;
+                    playerRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
+                }
+
+
+                if (Input.GetButton("Fire1") && !isRolling)
+                {
+                    anim.SetBool("isShooting", true);
+                    if (Time.time > fireRate + lastShot)
+                    {
+                        audioSource.PlayOneShot(gunSound, 0.35f);
+                        GameObject clone = Instantiate(laserShot, firePoint.position, firePoint.rotation);
+                        Physics2D.IgnoreCollision(clone.GetComponent<Collider2D>(), playerRB.GetComponent<Collider2D>());
+                        Rigidbody2D shot = clone.GetComponent<Rigidbody2D>();
+                        if (!m_FacingRight)
+                            shot.AddForce(transform.right * bulletSpeed, ForceMode2D.Impulse);
+                        else
+                            shot.AddForce(-transform.right * bulletSpeed, ForceMode2D.Impulse);
+                        Destroy(clone.gameObject, 1f);
+                        lastShot = Time.time;
+                    }
+                }
+                else
+                {
+                    anim.SetBool("isShooting", false);
+                }
+
+
+                if (Input.GetButtonUp("Fire2"))
+                {
                     canRollTimer = canRollTimeLeft;
                 }
+                if (Input.GetButton("Fire2") && canRollTimer > 0 && canRollCoolDown < 0)
+                {
+                    canRollTimer -= Time.deltaTime;
+                    anim.SetBool("isRolling", true);
+                    isRolling = true;
+                    if (canRollTimer < 0)
+                    {
+                        canRollCoolDown = 1f;
+                        canRollTimer = canRollTimeLeft;
+                    }
 
 
+                }
+                else
+                {
+                    canRollCoolDown -= Time.deltaTime;
+                    anim.SetBool("isRolling", false);
+                    isRolling = false;
+                }
+                if (Input.GetButton("Fire3"))
+                {
+                    moveSpeed = 16f;
+                }
+                else
+                {
+                    moveSpeed = 8f;
+                }
+
             }
-            else
-            {
-                canRollCoolDown -= Time.deltaTime;
-                anim.SetBool("isRolling", false);
-                isRolling = false;
-            }
-            if (Input.GetButton("Fire3"))
-            {
-                moveSpeed = 16f;
-            }
-            else 
-            { 
-                moveSpeed = 8f; 
-            }
-                  
-        }}
+        }
 
 
         if (isInAir)
         {
             inAirCounter += Time.deltaTime;
         }
-        if(playerHealthController.currentHealth <= 0)
+        if (playerHealthController.currentHealth <= 0)
         {
             isDead = true;
             anim.SetBool("isDead", true);
         }
-        if(isDead == true)
+        if (isDead == true)
         {
             isDeadCounter += Time.deltaTime;
             deathText.transform.position = playerRB.transform.position;
             deathText.gameObject.SetActive(true);
             playerRB.constraints = RigidbodyConstraints2D.FreezeRotation;
             playerRB.constraints = RigidbodyConstraints2D.FreezePosition;
-            if(isDeadCounter > 5)
+            if (isDeadCounter > 5)
             {
                 SceneManager.LoadScene(activeSceneIndex);
             }
@@ -175,7 +177,7 @@ public class PlayerController : MonoBehaviour
         {
             jumpCounter = 0;
             isInAir = false;
-            if(inAirCounter > 0.4f)
+            if (inAirCounter > 0.4f)
             {
                 playerHealthController.currentHealth = playerHealthController.currentHealth - (int)inAirCounter * 2;
                 playerHealthController.healthSlider.value = playerHealthController.currentHealth;
@@ -186,8 +188,8 @@ public class PlayerController : MonoBehaviour
                 inAirCounter = 0;
             }
 
-            
-            
+
+
         }
     }
     private void OnCollisionExit2D(Collision2D other)
