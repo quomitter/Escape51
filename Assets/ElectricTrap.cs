105fbb9341685849083f846f0cd3888053f6f88a
quomitter <109629602+quomitter@users.noreply.github.com>
2022-09-13 00:30:03 -0400

    changes

---
+++ Assets/ElectricTrap.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricTrap : MonoBehaviour
+{
+    PlayerHealthController playerHealthController; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealthController = FindObjectOfType<PlayerHealthController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "Player"){
+            
+            playerHealthController.DamagePlayer(2); 
+        }
+    }
+}
